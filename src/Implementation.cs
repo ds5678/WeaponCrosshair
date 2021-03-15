546a3134059a7ac5e23ebeb2513cc5be61539455
ds5678 <jrpritts7@gmail.com>
2021-03-15 13:31:05 -0400

    Add project files.

---
+++ src/Implementation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using Harmony;
+
+namespace WeaponCrosshair
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+            Settings.OnLoad();
+        }
+    }
+
+    [HarmonyPatch(typeof(HUDManager), "UpdateCrosshair")]
+    public class WeaponCrosshairUpdate
+    {
+        public static void Postfix(HUDManager __instance)
+        {
+            if (GameManager.GetPlayerManagerComponent().PlayerIsZooming())
+            {
+                GearItem itemInHands = GameManager.GetPlayerManagerComponent().m_ItemInHands;
+                bool showForStoneItem = (Settings.options.stoneCrosshair && itemInHands.m_StoneItem);
+                bool showForGunItem = (Settings.options.rifleCrosshair && itemInHands.m_GunItem);
+                bool showForBowItem = (Settings.options.bowCrosshair && itemInHands.m_BowItem);
+                if ( showForStoneItem || showForGunItem || showForBowItem )
+                {
+                    //MelonLoader.MelonLogger.Log("Attempting to show crosshair");
+                    Utils.SetActive(InterfaceManager.m_Panel_HUD.m_Sprite_Crosshair.gameObject, true);
+                    InterfaceManager.m_Panel_HUD.m_Sprite_Crosshair.alpha = 1f;
+                }
+            }
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModSettings;
+
+namespace WeaponCrosshair
+{
+    internal class WeaponCrosshairSettings : JsonModSettings
+    {
+        [Name("Stone crosshair")]
+        [Description("Default value is false")]
+        public bool stoneCrosshair = false;
+
+        [Name("Rifle crosshair")]
+        [Description("Default value is false")]
+        public bool rifleCrosshair = false;
+
+        [Name("Bow crosshair")]
+        [Description("Default value is false")]
+        public bool bowCrosshair = false;
+    }
+    internal class Settings
+    {
+        internal static WeaponCrosshairSettings options;
+        internal static void OnLoad()
+        {
+            options = new WeaponCrosshairSettings();
+            options.AddToModSettings("Weapon Crosshair");
+        }
+    }
+}
